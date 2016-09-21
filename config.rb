2749e2ec7e3f37d138896750351c729ed5e92744
Jesse McGinnis <jesse@jcmcginnis.com>
2016-09-21 18:44:21 -0400

    Upgrade to middleman 4

--- config.rb
+++ config.rb
@@ -1,37 +1,35 @@
-Slim::Engine.disable_option_validator!
+activate :directory_indexes
+activate :autoprefixer
+activate :automatic_image_sizes
 
-###
-# Helpers
-###
+set :css_dir,          "assets/stylesheets"
+set :js_dir,           "assets/javascripts"
+set :images_dir,       "assets/images"
+set :fonts_dir,        "assets/fonts"
+set :layout,           "layouts/application"
+set :relative_links,   true
+
+page "/*.xml", layout: false
+page "/*.json", layout: false
+page "/*.txt", layout: false
+
+configure :development do
+  activate :livereload
+end
+
+configure :build do
+  activate :minify_css
+  activate :relative_assets
+end
+
+activate :deploy do |deploy|
+  deploy.build_before = true
+  deploy.deploy_method = :git
+end
 
 helpers do
   def external_link_to(body, url, html_options={})
     html_options[:rel] ||= 'external'
     link_to body, url, html_options
   end
-end
-
-###
-# Settings
-###
-
-set :css_dir,    'stylesheets'
-set :js_dir,     'javascripts'
-set :images_dir, 'images'
-
-# Automatic image dimensions on image_tag helper
-activate :automatic_image_sizes
-
-# Reload the browser automatically whenever files change
-configure :development do
-  activate :livereload
-end
-
-# Build-specific configuration
-configure :build do
-  activate :minify_html
-  activate :minify_css
-  activate :minify_javascript
-  activate :asset_hash
-  activate :relative_assets
 end
